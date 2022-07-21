9825dc69579103098534c205638e0c479c51e3a3
Losos, Radek (ADV D EU CZ AE CV 5) <radek.losos@siemens.com>
2022-07-21 21:46:32 +0200

    write text to file

--- gRPC_server/src/main.rs
+++ gRPC_server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main()  -> std::io::Result<()>{
+    let mut buffer = File::create("foo.txt")?;
+
+    // Writes some prefix of the byte string, not necessarily all of it.
+    buffer.write(b"some bytes")?;
+    Ok(())
+}
+
+fn write_text_to_file(file : String, text:String){
+
 }

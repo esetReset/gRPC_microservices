8af5a2144686caf781465cc55cff1c02c677eaa7
Losos, Radek (ADV D EU CZ AE CV 5) <radek.losos@siemens.com>
2022-07-22 06:58:33 +0200

    get compilable version of writing text to file

--- gRPC_server/src/main.rs
+++ gRPC_server/src/main.rs
@@ -1,14 +1,17 @@
 use std::io::prelude::*;
 use std::fs::File;
+use std::path::Path;
 
-fn main()  -> std::io::Result<()>{
-    let mut buffer = File::create("foo.txt")?;
-
-    // Writes some prefix of the byte string, not necessarily all of it.
-    buffer.write(b"some bytes")?;
-    Ok(())
+fn main()  {
+   let file_path = Path::new("./target/foo.txt");
+   let text = String::from("test bytes");
+   write_text_to_file(file_path, text).expect("Could not write to file {file_path}");
 }
 
-fn write_text_to_file(file : String, text:String){
+fn write_text_to_file(file: &Path, text:String) -> std::io::Result<()>{
+   let mut buffer = File::create(file.as_os_str())?;
 
+   // Writes some prefix of the byte string, not necessarily all of it.
+   buffer.write( text.as_bytes())?;
+   Ok(())
 }
